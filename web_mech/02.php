d33727483dcc76cf7c5da9ef89f63f367da99aeb
SergejsRom <sergejsromanovskis@gmail.com>
2022-05-30 23:56:52 +0300

    02 hw

---
+++ web_mech/02.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>||| 01 |||</title>
+    <link rel="stylesheet" href="./01.css">
+</head>
+<body class="juodas"
+
+<?php
+$sp = $_GET['spalva'];
+if ($sp !== null) {
+
+   echo "style='background-color: #{$sp}'";
+
+}
+?>
+>
+    <a class="baltas" href="http://localhost/php-2/web_mech/02.php">BE GET</a>
+    <br>
+    <br>
+    <form action="" method="get">
+
+        <span class="baltas">Laukas spalvai:</span> <input type="text" name="spalva">
+        <button type="submit">FONO SPALVA</button>
+
+    </form>
+</body>
+</html>
