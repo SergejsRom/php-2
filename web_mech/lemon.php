e79da6adec0c08b53627765cdd6168cd92f3f7f4
SergejsRom <sergejsromanovskis@gmail.com>
2022-05-31 00:11:58 +0300

    04 hw

---
+++ web_mech/lemon.php
@@ -0,0 +1,4 @@
+<body style="background-color: yellow">
+<a href="http://localhost/php-2/web_mech/orange.php">I Orange</a>
+
+</body>
---
+++ web_mech/orange.php
@@ -0,0 +1,3 @@
+<body style="background-color: orange">
+<a href="http://localhost/php-2/web_mech/lemon.php"> I LEMON</a>
+</body>
