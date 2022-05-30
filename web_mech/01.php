8a39ad4283a60ff8c2aec4fbf7f8ee5a5d6df147
SergejsRom <sergejsromanovskis@gmail.com>
2022-05-30 21:27:05 +0300

    01 hw

---
+++ web_mech/01.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>||| 01 |||</title>
+    <link rel="stylesheet" href="./01.css">
+</head>
+<body class="juodas 
+<?php
+if ($_GET['color'] == 1) {
+    ?>
+    return raudonas
+    <?php
+}
+
+?>" 
+
+
+
+>
+    <a class="baltas" href="http://localhost/php-2/web_mech/01.php">BE GET</a>
+    <br>
+    <br>
+    <a class="baltas" href="http://localhost/php-2/web_mech/01.php?color=1">SU GET</a>
+</body>
+</html>
