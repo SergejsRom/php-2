a45964a15ed56df4cc0e57707ad8ad50524c50c3
SergejsRom <sergejsromanovskis@gmail.com>
2022-06-16 12:25:45 +0300

    r2

---
+++ r2/src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.css';
+
+function App() {
+  return (
+    <div className="container">
+    <div className="row">
+      
+      <div className="col-7">
+        One of three columns
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
---
+++ r2/src/Components/create.jsx
@@ -0,0 +1,19 @@
+function Create() {
+    return (
+        <div className="col-5">
+        <div calssName="card">
+  <div calssName="card-header">
+    Featured
+  </div>
+  <div calssName="card-body">
+    <h5 calssName="card-title">Special title treatment</h5>
+    <p calssName="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" calssName="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+      </div>
+    );
+  }
+  
+  export default Create;
+  
